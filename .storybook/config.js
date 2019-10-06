243e56b5d20b8bc95d84850b69d35b3613c93ac9
sebastain <eskrzy@gmail.com>
2019-10-06 15:47:53 +0900

    init

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../src/shared/global';
+
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src', true, /\.stories\.js$/), module);
---
+++ src/Avatar.stories.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+
+import { Avatar } from './Avatar';
+
+export default {
+  title: 'Design System|Avatar',
+
+  parameters: {
+    component: Avatar,
+  },
+};
+
+export const standard = () => (
+  <Avatar
+    size="large"
+    username="Tom Coleman"
+    src="https://avatars2.githubusercontent.com/u/132554"
+  />
+);
+
+export const sizes = () => (
+  <div>
+    <Avatar
+      size="large"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+    <Avatar
+      size="medium"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+    <Avatar
+      size="small"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+    <Avatar
+      size="tiny"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+  </div>
+);
+
+export const initials = () => (
+  <div>
+    <Avatar username="Tom Coleman" />
+    <Avatar username="Dominic Nguyen" />
+    <Avatar username="Kyle Suss" />
+    <Avatar username="Michael Shilman" />
+  </div>
+);
+
+export const loading = () => (
+  <div>
+    <Avatar size="large" loading />
+    <Avatar size="medium" loading />
+    <Avatar size="small" loading />
+    <Avatar size="tiny" loading />
+  </div>
+);
+
+export const large = () => (
+  <div>
+    <Avatar loading size="large" />
+    <Avatar size="large" username="Tom Coleman" />
+    <Avatar
+      size="large"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+  </div>
+);
+
+export const knobs = () => (
+  <Avatar
+    loading={boolean('Loading')}
+    size={select('Size', ['tiny', 'small', 'medium', 'large'])}
+    username="foo"
+    src="https://avatars2.githubusercontent.com/u/263385"
+  />
+);
+
+knobs.story = {
+  decorators: [withKnobs],
+};
