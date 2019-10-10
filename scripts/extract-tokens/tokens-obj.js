e68ea023508a1f484b6a97e161c0e5df77c0099f
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-10 11:51:07 +0900

    refactor: simplify scripts

--- scripts/extract-tokens/tokens-obj.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-import { tokensFilePath, buildTokensObj } from '../utils';
-
-buildTokensObj().then(values => {
-  fs.writeFileSync(tokensFilePath, JSON.stringify(values));
-});
