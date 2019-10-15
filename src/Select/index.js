8245df8aacc35ef9d61a0edbb343fca43de7780f
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-15 20:38:06 +0900

    feat(select): support error and loading states

--- src/Select/index.js
+++ src/Select/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Downshift from 'downshift';
 import cx from 'classnames';
-import { arrayOf, func, string, number, shape } from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
+import { arrayOf, bool, func, string, number, shape } from 'prop-types';
 
 import List from './List';
 import FieldOverlap from '../FieldOverlap';
 
 import styles from './styles.module.scss';
 
-const Select = ({ items, name, onSelect, width }) => {
+const Select = ({
+  error,
+  items,
+  name,
+  label,
+  loading,
+  onSelect,
+  onInputReset,
+  width,
+}) => {
   return (
     <Downshift
       onChange={selection => {
@@ -25,56 +35,85 @@ const Select = ({ items, name, onSelect, width }) => {
         highlightedIndex,
         selectedItem,
         openMenu,
+        inputValue,
         closeMenu,
-      }) => (
-        <div style={{ width }}>
-          <FieldOverlap label={name} width={width} {...getLabelProps()}>
-            <div
-              className={styles.inputWrapper}
-              role="textbox"
-              aria-label="input"
+        clearSelection,
+      }) => {
+        const resetIcon = !!(inputValue && !isEmpty(inputValue));
+        const handleReset = () => {
+          if (onInputReset) onInputReset(name, '');
+          clearSelection();
+        };
+
+        return (
+          <div style={{ width }}>
+            <FieldOverlap
+              label={label}
+              width={width}
+              error={error}
+              loading={loading}
+              name={name}
+              resetIcon={resetIcon}
+              onReset={handleReset}
+              {...getLabelProps()}
             >
-              <input
-                {...getInputProps()}
-                onClick={openMenu}
-                type="search"
-                data-testid="select input"
-                className={styles.input}
-                placeholder="ex. Foo, Bar..."
-                name={name}
-              />
-              <button
-                data-testid="select icon"
-                className={cx(styles.icon, isOpen && styles.iconOpen)}
-                onClick={!isOpen ? openMenu : closeMenu}
+              <div
+                className={styles.inputWrapper}
+                role="textbox"
+                aria-label="input"
               >
-                ❯
-              </button>
-            </div>
-            <List
-              items={items}
-              getMenuProps={getMenuProps}
-              getItemProps={getItemProps}
-              highlightedIndex={highlightedIndex}
-              selectedItem={selectedItem}
-              isOpen={isOpen}
-            />
-          </FieldOverlap>
-        </div>
-      )}
+                <input
+                  {...getInputProps()}
+                  onClick={openMenu}
+                  data-testid="select input"
+                  className={styles.input}
+                  placeholder="ex. Foo, Bar..."
+                  name={name}
+                />
+                <button
+                  data-testid="select icon"
+                  className={cx(styles.icon, isOpen && styles.iconOpen)}
+                  onClick={!isOpen ? openMenu : closeMenu}
+                >
+                  ❯
+                </button>
+              </div>
+              <List
+                resetIcon={resetIcon}
+                error={error}
+                loading={loading}
+                items={items}
+                getMenuProps={getMenuProps}
+                getItemProps={getItemProps}
+                highlightedIndex={highlightedIndex}
+                selectedItem={selectedItem}
+                isOpen={isOpen}
+              />
+            </FieldOverlap>
+          </div>
+        );
+      }}
     </Downshift>
   );
 };
 
 Select.propTypes = {
+  error: string,
   items: arrayOf(shape({})),
+  label: string,
+  loading: bool,
   name: string,
+  onInputReset: func,
   onSelect: func,
   width: number,
 };
 Select.defaultProps = {
+  error: null,
   items: [],
+  label: 'Select',
+  loading: false,
   name: 'single select',
+  onInputReset: null,
   onSelect: null,
   width: 200,
 };
