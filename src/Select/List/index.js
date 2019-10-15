3bd394be05684cb07a90f0ced3810abe757d8746
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-15 15:43:15 +0900

    feat(select): support loading and error states in select's list

--- src/Select/List/index.js
+++ src/Select/List/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { arrayOf, bool, func, number, shape } from 'prop-types';
+import { arrayOf, bool, func, number, string, shape } from 'prop-types';
 import cx from 'classnames';
 import ListItem from '../ListItem';
 import styles from '../styles.module.scss';
 
 const List = ({
+  error,
   items,
   getMenuProps,
   isOpen,
+  loading,
   highlightedIndex,
   getItemProps,
+  resetIcon,
   selectedItem,
 }) => {
   const listClasses = {
     [styles.list]: true,
     [styles.listVisible]: isOpen,
     [styles.listHidden]: !isOpen,
+    [styles.listWithIcon]:
+      (!error && loading) || (error && !loading) || resetIcon,
+    [styles.listWithDoubleIcon]: (error || loading) && resetIcon,
   };
 
   return (
@@ -43,17 +49,23 @@ const List = ({
 };
 
 List.propTypes = {
+  error: string,
   items: arrayOf(shape({})),
   getMenuProps: func.isRequired,
   isOpen: bool.isRequired,
+  loading: bool,
   highlightedIndex: number,
   getItemProps: func.isRequired,
   selectedItem: shape({}),
+  resetIcon: bool,
 };
 
 List.defaultProps = {
+  error: null,
   items: [],
   highlightedIndex: null,
+  loading: false,
+  resetIcon: false,
   selectedItem: null,
 };
 
