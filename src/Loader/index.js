d5f4877d810d6f9a52adec7d2eb817f3c11c49ac
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-15 15:19:13 +0900

    refactor(loader): support more props flexibility

--- src/Loader/index.js
+++ src/Loader/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { number } from 'prop-types';
+import cx from 'classnames';
+import { number, string } from 'prop-types';
 import styles from './styles.module.scss';
 
-const Loader = ({ size, thickness }) => {
+const Loader = ({ size, thickness, className, ...props }) => {
   const loaderStyles = {
     width: size,
     height: size,
     borderWidth: thickness,
   };
   return (
-    <div className={styles.loader} style={loaderStyles} aria-label="loader" />
+    <div
+      className={cx(styles.loader, className)}
+      style={loaderStyles}
+      aria-label="loader"
+      {...props}
+    />
   );
 };
 
 Loader.propTypes = {
+  className: string,
   size: number,
   thickness: number,
 };
 Loader.defaultProps = {
+  className: '',
   size: 30,
   thickness: 3,
 };
