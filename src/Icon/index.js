eb616fdcb67246fa83e7c1b1111f1dd864943907
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-18 18:20:39 +0900

    feat(icon): add classnames prop and visible related classes

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import cx from 'classnames';
 import { icons } from '../shared/icons';
+
+import styles from './styles.module.scss';
 
 const Svg = styled.svg`
   display: ${props => (props.block ? 'block' : 'inline-block')};
@@ -26,13 +29,20 @@ const PathSecondary = styled.path`
  * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
  * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
  */
-const Icon = ({ icon, block, ...props }) => {
+const Icon = ({ icon, block, visible, className, ...props }) => {
+  const iconClasses = {
+    [styles.icon]: true,
+    [styles.visible]: visible,
+    [styles.notVisible]: !visible,
+  };
+
   return (
     <Svg
       viewBox="0 0 24 24"
       width="20px"
       height="20px"
       block={block}
+      className={cx(iconClasses, className)}
       {...props}
     >
       {typeof icons[icon] === 'object' ? (
@@ -48,12 +58,16 @@ const Icon = ({ icon, block, ...props }) => {
 };
 
 Icon.propTypes = {
+  className: PropTypes.string,
   icon: PropTypes.string.isRequired,
   block: PropTypes.bool,
+  visible: PropTypes.bool,
 };
 
 Icon.defaultProps = {
+  className: null,
   block: false,
+  visible: true,
 };
 
 export default Icon;
