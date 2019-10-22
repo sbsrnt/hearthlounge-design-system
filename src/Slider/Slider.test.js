958460ef7482971b91b1eb370b4b81ca56d94e89
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-22 17:41:18 +0900

    test(slider): new component

---
+++ src/Slider/Slider.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Slider from '.';
+
+describe('Slider Component', () => {
+  test('renders correctly', async () => {
+    const { container } = render(<Slider />);
+
+    expect(container).not.toBeNull();
+  });
+});
