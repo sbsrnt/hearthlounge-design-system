06a35aac54ac099265c184cef5634452a352ab2d
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-22 17:40:47 +0900

    feat(slider): new component

---
+++ src/Slider/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import RcSlider from 'rc-slider';
+import { func, number, string } from 'prop-types';
+import 'rc-slider/assets/index.css';
+
+const Slider = ({ name, onChange, value, ...rest }) => {
+  const handleChange = () => v => onChange(name, v);
+  const props = {
+    name,
+    onChange: onChange ? handleChange : null,
+    ...(value && { value }),
+    ...rest,
+  };
+  return <RcSlider {...props} />;
+};
+
+Slider.propTypes = {
+  name: string,
+  onChange: func,
+  value: number,
+};
+
+Slider.defaultProps = {
+  name: 'slider',
+  onChange: null,
+  value: 0,
+};
+
+export default Slider;
