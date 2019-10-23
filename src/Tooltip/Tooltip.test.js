5597c442df2fbe94154fdb7206e28ef2ff53090e
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-10-23 22:00:04 +0900

    test(tooltip): applyChildStyles prop

--- src/Tooltip/Tooltip.test.js
+++ src/Tooltip/Tooltip.test.js
@@ -3,10 +3,24 @@ import { render } from '@testing-library/react';
 
 import Tooltip from '.';
 
+import styles from './styles.module.scss';
+
 describe('Tooltip Component', () => {
   test('renders correctly', async () => {
     const { container } = render(<Tooltip text="foo">bar</Tooltip>);
 
     expect(container).not.toBeNull();
+  });
+
+  test('has tooltipText class on applyChildStyles prop', async () => {
+    const { container } = render(
+      <Tooltip text="foo" applyStylesToChild>
+        bar
+      </Tooltip>
+    );
+
+    expect(
+      container.firstChild.classList.contains(styles.tooltipText)
+    ).toBeTruthy();
   });
 });
