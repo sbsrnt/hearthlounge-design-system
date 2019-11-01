0b0bdd8b7d3dcd2c7d6a37b4a3f77d40e52390b0
Sebastian Krzyżanowski <eskrzy@gmail.com>
2019-11-01 15:17:06 +0900

    feat(icon): add 3 static sizes to icon

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bool, number, string } from 'prop-types';
+import { bool, number, oneOfType, oneOf, string } from 'prop-types';
 import cx from 'classnames';
 import { icons } from '../icons';
 
@@ -10,10 +10,23 @@ const Icon = ({ name, block, className, size, ...props }) => {
     icon__block: block,
   };
 
+  const getSize = () => {
+    switch (size) {
+      case 'xs':
+        return 16;
+      case 'md':
+        return 24;
+      case 'lg':
+        return 35;
+      default:
+        return size;
+    }
+  };
+
   return (
     <svg
-      width={size}
-      height={size}
+      width={getSize()}
+      height={getSize()}
       viewBox="0 0 1024 1024"
       className={cx(iconClasses, className)}
       {...props}
@@ -26,6 +39,8 @@ const Icon = ({ name, block, className, size, ...props }) => {
   );
 };
 
+const staticSizes = ['xs'];
+
 Icon.propTypes = {
   block: bool,
   /** Any additional classNames to specify on the element */
@@ -33,7 +48,7 @@ Icon.propTypes = {
   /** Name of the icon to be rendered */
   name: string.isRequired,
   /** Size of the icon */
-  size: number,
+  size: oneOfType([oneOf(staticSizes), number]),
 };
 
 Icon.defaultProps = {
